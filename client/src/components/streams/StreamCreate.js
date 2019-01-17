c5c60bdef9ecb495570f58f09d1c37594a78f4ca
John Pawlak <johnpawlak@gmail.com>
2019-01-16 21:47:26 -0500

    added validation for form component

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -4,12 +4,15 @@ import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends React.Component {
   //formprops have all the functions (onChange etc,) also value, but it is up to us to make the final connections
-  //we are destructuring formProps now instead of adding {...formProps.input} into <input />
-  renderInput({ input, label }) {
+  //we are destructuring formProps now instead of adding {...formProps.input} into <input /> meta holds errors, label keeps track of field labels
+  renderInput = ({ input, label, meta }) => {
+    // console.log(meta)
     return(
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {meta.touched &&
+          (meta.error && <span>{meta.error}</span>)}
       </div>
     )
   }
@@ -24,7 +27,7 @@ class StreamCreate extends React.Component {
     //component needs to be shown, so you need to have the component made in advance and pass it in
     // console.log(this.props)
     return ( 
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" component={this.renderInput} label="Enter Title"/>
         <Field name="description" component={this.renderInput} label = "Enter Description"/>
         <button className="ui button primary">Submit</button>
@@ -32,9 +35,22 @@ class StreamCreate extends React.Component {
     )
   }
 }
-  
 
+//the way redux-form validates form values
+const validate = (values) => {
+  const errors = {}
+  if(!values.title) {
+    //only runs if user does not enter title
+    errors.title = "Title is Required"
+  }
+  if(!values.description) {
+    errors.description = "Description is Required"
+  }
+  return errors
+}
+
+//links this component to redux state by using reduxForm the same way connect does. It sets up streamCreate inside form state
 export default reduxForm({
-  //value of form is usually just a name for what it is being used
-  form: 'streamCreate'
+  form: 'streamCreate', // a unique identifier for this form
+  validate: validate, // <--- validation function given to redux-form
 })(StreamCreate)
