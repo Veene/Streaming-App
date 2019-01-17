c67a57794ae882f16a8de794834de09f9fbc76ba
John Pawlak <johnpawlak@gmail.com>
2019-01-17 13:04:24 -0500

    added api server and hooked up json-server for REST-ful server and local db ease

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
-import { SIGN_IN, SIGN_OUT } from './types'
+import streams from '../apis/stream'
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, DELETE_STREAM, FETCH_STREAM, FETCH_STREAMS, EDIT_STREAM } from './types'
 
 export const signIn = (id) => {
   return {
@@ -10,4 +11,42 @@ export const signOut = () => {
   return {
     type: SIGN_OUT
   }
+}
+
+//this is redux thunk boilerplate for action creator
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    const response = await streams.post('/streams', formValues)
+
+    //here we are directly dispatching instead of calling dispatch(action -> which returns the type:'' payload:'')
+    dispatch({
+      type: CREATE_STREAM,
+      payload: response.data
+    })
+  }
+}
+//get list of streams
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get('/streams')
+
+  dispatch( { type: FETCH_STREAMS, payload: response.data})
+}
+//getting only 1 single stream (need to know ID)
+export const fetchStream = (id) => async dispatch => {
+  const response = await streams.get(`/streams/${id}`)
+
+  dispatch( { type: FETCH_STREAM, payload: response.data})
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues)
+
+  dispatch({ type: EDIT_STREAM, payload: response.data })
+}
+
+export const deleteStream = (id) => async dispatch => {
+  //dont needs a repsonse because it will be empty because of delete
+  await streams.delete(`/streams/${id}`)
+
+  dispatch({ type: DELETE_STREAM, payload: id})
 }
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,6 +1,8 @@
 import React from 'react'
 //reduxForm is a function similar to connect() -> allows mapstatetoprops and action creators
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { createStream } from '../../actions'
 
 class StreamCreate extends React.Component {
   //formprops have all the functions (onChange etc,) also value, but it is up to us to make the final connections
@@ -16,11 +18,12 @@ class StreamCreate extends React.Component {
       </div>
     )
   }
-  onSubmit(formValues) {
+  onSubmit = (formValues) => {
     //redux-form handles the event and preventDefault, so we dont have to worry about it so basically we use the builtin handleSubmit
     //which we get on this whole file because of reduxForm connect() style. handleSubmit throws the values entered back as object
     //{title:EXAMPLE, description:EXAMPLE}
     console.log(formValues)
+    this.props.createStream(formValues)
   }
   render() {
     //TONS of different props from the redux-form lib, pick the right one //Reminder! Field needs its attributes name, etc,etc component is necessary
@@ -50,7 +53,9 @@ const validate = (values) => {
 }
 
 //links this component to redux state by using reduxForm the same way connect does. It sets up streamCreate inside form state
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate', // a unique identifier for this form
   validate: validate, // <--- validation function given to redux-form
 })(StreamCreate)
+
+export default connect(null, { createStream })(formWrapped)
